84e861938ecaab50c85162200b30afbb30e5b79d
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-04-27 19:26:48 -0300

    secondary animation created

--- src/Screens/Secondary/index.tsx
+++ src/Screens/Secondary/index.tsx
@@ -1,11 +1,71 @@
 import React from "react";
+import {
+    PanGestureHandler,
+    PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import {
+    useAnimatedGestureHandler,
+    useAnimatedStyle,
+    useSharedValue,
+    withSpring,
+} from "react-native-reanimated";
 
-import { Box, Container } from "./styles";
+import { Box, Circle, Container } from "./styles";
+
+type ContextType = {
+    translateX: number;
+    translateY: number;
+};
 
 export const Secondary = () => {
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+
+    // Usamos o hook para controlar o pangestureEvent
+    const panGestureEvent = useAnimatedGestureHandler<
+        PanGestureHandlerGestureEvent,
+        ContextType
+    >({
+        onStart: (event, context) => {
+            context.translateX = translateX.value;
+            context.translateY = translateY.value;
+        },
+        onActive: (event, context) => {
+            translateX.value = event.translationX + context.translateX;
+            translateY.value = event.translationY + context.translateY;
+        },
+        onEnd: () => {
+            const distance = Math.sqrt(
+                translateX.value ** 2 + translateY.value ** 2
+            );
+
+            if (distance < 200.0 + 100 / 2) {
+                translateX.value = withSpring(0);
+                translateY.value = withSpring(0);
+            }
+        },
+    });
+
+    const reanimatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateX: translateX.value,
+                },
+                {
+                    translateY: translateY.value,
+                },
+            ],
+        };
+    });
+
     return (
         <Container>
-            <Box />
+            <Circle>
+                <PanGestureHandler onGestureEvent={panGestureEvent}>
+                    <Box style={reanimatedStyle} />
+                </PanGestureHandler>
+            </Circle>
         </Container>
     );
 };
--- src/Screens/Secondary/styles.ts
+++ src/Screens/Secondary/styles.ts
@@ -1,3 +1,4 @@
+import Animated from "react-native-reanimated";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -6,8 +7,19 @@ export const Container = styled.View`
     align-items: center;
 `;
 
-export const Box = styled.View`
+export const Circle = styled.View`
+    height: 350px;
+    width: 350px;
+    border-radius: 175px;
+    align-items: center;
+    justify-content: center;
+    border-width: 5px;
+    border-color: blue;
+`;
+
+export const Box = styled(Animated.View)`
     width: 100px;
     height: 100px;
     background-color: blue;
+    border-radius: 20px;
 `;
