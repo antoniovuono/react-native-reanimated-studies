c3eef9c1a2c9eab6c23baa1fa5e09b911d5cf1fb
Antonio Vuono <antoniosvuono@icloud.com>
2023-05-01 23:32:47 -0300

    Animations from border radius

--- src/components/Pages/index.tsx
+++ src/components/Pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Dimensions } from "react-native";
 import Animated, {
+    Extrapolate,
     interpolate,
     useAnimatedStyle,
 } from "react-native-reanimated";
@@ -14,16 +15,31 @@ interface PagesProps {
 }
 
 const { height, width } = Dimensions.get("window");
+const SIZE = width * 0.7;
 
 export const Pages = ({ title, index, translateX }: PagesProps) => {
+    const inputRange = [
+        (index - 1) * width,
+        index * width,
+        (index + 1) * width,
+    ];
+
     const rStyle = useAnimatedStyle(() => {
         const scale = interpolate(
             translateX.value,
-            [(index - 1) * width, index * width, (index + 1) * width],
-            [0, 1, 0]
+            inputRange,
+            [0, 1, 0],
+            Extrapolate.CLAMP
         );
 
+        const borderRadius = interpolate(translateX.value, inputRange, [
+            0,
+            SIZE / 2,
+            0,
+        ]);
+
         return {
+            borderRadius,
             transform: [{ scale }],
         };
     });
