97be7853f35cb3ef11d6b461a69bd1fe76112212
Antonio Vuono <antoniosvuono@icloud.com>
2023-05-06 10:26:44 -0300

    removing console log

--- src/Screens/ExerciseList/components/Contact/index.tsx
+++ src/Screens/ExerciseList/components/Contact/index.tsx
@@ -42,7 +42,6 @@ export const Contact = ({ name, phone }: IContactInfo) => {
                     transalateX.value = withSpring(0);
                 } else {
                     transalateX.value = event.translationX;
-                    console.log(transalateX.value);
                 }
             },
             onEnd: () => {
